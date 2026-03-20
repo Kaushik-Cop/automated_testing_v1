b0b7e09177c605c7d459aa2a33e14afbadaef2d1
John Robertson <john@myjamjar.com.au>
2026-03-20 11:26:18 +0800

    chore: pass the linting errors

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b 
 
+
 def multiply(a, b):
     return a * b
+
 
 def convert_fahrenheit_to_celsius(fahrenheit):
     return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
